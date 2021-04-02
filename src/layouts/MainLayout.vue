b81bd0eaf5ae0ce8121b4977128f98218ded2730
Thayana Mamoré <thayanacmamore@gmail.com>
2021-04-02 15:22:10 -0300

    Start building Quasagram - Layout, pages and routes

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          to="/camera"
+          class="large-screen-only q-mr-sm"
+          icon="eva-camera-outline"
+          size="18px"
+          flat
+          round
+          dense
+        />
+
+        <q-separator
+          class="large-screen-only"
+          spaced
+          vertical
+        />
+
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasagram
+        </q-toolbar-title>
+
+        <q-btn
+          to="/"
+          class="large-screen-only"
+          icon="eva-home-outline"
+          size="18px"
+          flat
+          round
+          dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab icon="eva-home-outline" to="/" />
+        <q-route-tab icon="eva-camera-outline" to="/camera" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height: 70px
+
+.q-toolbar__title
+  font-size: 30px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
+
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+</style>
